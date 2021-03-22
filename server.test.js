40acde66b69643d4006033a28f189b63afcbc4f0
NicSantiago1 <57651231+NicSantiago1@users.noreply.github.com>
2021-03-22 18:49:21 -0400

    Fixed package file to include testing file

--- server.test.js
+++ server.test.js
@@ -2,8 +2,6 @@ var expect = require("chai").expect;
 var assert = require('assert');
 var request = require("request");
 
-var listings;
-var index = require('../Homegrown-Alligators/src/index.js');
 
 describe('Website server Unit Tests', function() {
     it('should respond', function(done) {
@@ -15,12 +13,5 @@ describe('Website server Unit Tests', function() {
             }
             done();
         });        
-    });
-    it('responds with a 404 error to other GET requests', function(done) {
-        request.get('http://localhost:3000/About', function(error, response, body) {
-          assert.equal(response.statusCode, 404);
-          assert.equal(body, '404, Page Not Found');
-          done();
-        });
     });
 });
