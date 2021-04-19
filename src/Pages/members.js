376734c135285488c690c8148900326340ce716f
Elizabeth <echurinov@gmail.com>
2021-04-19 00:32:38 -0400

    Updated Members Page
    Changed formatting on Members Page to change how many members render per line when screen size is changed

--- src/Pages/members.js
+++ src/Pages/members.js
@@ -3,51 +3,51 @@ import Footer from "../components/footer";
 import PageTitle from "../components/pageTitle";
 import Header from "../components/header";
 import "./members.css";
-import '../index.css';
-import space2 from '../components/Images/space2.png';
+import "../index.css";
+import space2 from "../components/Images/space2.png";
 
-class Member extends React.Component
-{
-  constructor(props)
-  {
-    super(props)
+class Member extends React.Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
-      name:this.props.name,
-      position:this.props.position
-    }
+      name: this.props.name,
+      position: this.props.position,
+    };
   }
 }
 
 const members = [
-  <Member name="Tom Clubman"        position="President"/>,
-  <Member name="Berry Buildabear"   position="Vice President"/>,
-  <Member name="Publix Supermarket" position="Treasurer"/>,
-  <Member name="Bippity"            position="Bank Robber"/>,
-  <Member name="Boppity"            position="Bank Robber's Assistant"/>,
-  <Member name="Boo"                position="Bank Robber's Assistant's Assistant"/>,
-  <Member name="Shoe"               position="Closet Dweller"/>,
-  <Member name="Mr. White"          position="Chemistry Teacher"/>,
-  <Member name="Jesse"              position="Chemistry Teacher's Student"/>,
-  <Member name="Saul"               position="Chemistry Teacher's Lawyer"/>,
-  <Member name="Gus"                position="Chemistry Teacher's Employer"/>,
-  <Member name="Best Buy"           position="Technology Store"/>,
-  <Member name="Target"             position="Supermarket"/>,
-]
+  <Member name="Tom Clubman" position="President" />,
+  <Member name="Berry Buildabear" position="Vice President" />,
+  <Member name="Publix Supermarket" position="Treasurer" />,
+  <Member name="Bippity" position="Bank Robber" />,
+  <Member name="Boppity" position="Bank Robber's Assistant" />,
+  <Member name="Boo" position="Bank Robber's Assistant's Assistant" />,
+  <Member name="Shoe" position="Closet Dweller" />,
+  <Member name="Mr. White" position="Chemistry Teacher" />,
+  <Member name="Jesse" position="Chemistry Teacher's Student" />,
+  <Member name="Saul" position="Chemistry Teacher's Lawyer" />,
+  <Member name="Gus" position="Chemistry Teacher's Employer" />,
+  <Member name="Best Buy" position="Technology Store" />,
+  <Member name="Target" position="Supermarket" />,
+];
 
 class MemberBox extends React.Component {
   render() {
     return (
       <div className="member-box">
-        <div className="member-image">
-          <img src={this.props.imgPath} alt="" />
-        </div>
-        <div className="member-info">
-          <div className="member-name">
-            <p>{this.props.name}</p>
+        <div className="member-content">
+          <div className="member-image">
+            <img src={this.props.imgPath} alt="" />
           </div>
-          <div className="member-role">
-            <p>{this.props.role}</p>
+          <div className="member-info">
+            <div className="member-name">
+              <p>{this.props.name}</p>
+            </div>
+            <div className="member-role">
+              <p>{this.props.role}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -56,32 +56,33 @@ class MemberBox extends React.Component {
 }
 
 export default class Members extends React.Component {
-  renderMemberBoxes()
-  {
+  renderMemberBoxes() {
     let memberBoxes = [];
 
-    for(var i = 0; i < members.length; i++)
-    {
-      memberBoxes.push(<MemberBox name={members[i].props.name} role={members[i].props.position} imgPath="gators.png"/>);
+    for (var i = 0; i < members.length; i++) {
+      memberBoxes.push(
+        <MemberBox
+          name={members[i].props.name}
+          role={members[i].props.position}
+          imgPath="gators.png"
+        />
+      );
     }
 
     return memberBoxes;
   }
-  
-  render() {
 
+  render() {
     return (
       <div className="page">
         <Header />
 
         <div className="title-wrapper">
-          <img  src={space2} ></img>
+          <img src={space2}></img>
           <PageTitle text="MEET OUR MEMBERS" />
         </div>
 
-        <div className="member-grid">
-          {this.renderMemberBoxes()}
-        </div>
+        <div className="member-grid">{this.renderMemberBoxes()}</div>
         <div>
           <Footer />
         </div>
