765680181840a7cdb4cde677d58346cdd66baf36
Elizabeth <echurinov@gmail.com>
2021-04-20 16:20:47 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const uri = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      uri,
+      uri || db,
       {
         useNewUrlParser: true
       }
@@ -17,9 +18,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-if(provess.env.NODE_ENV === 'production'){
-  app.use(express.static('../build'));
-}
 
 module.exports = connectDB;
