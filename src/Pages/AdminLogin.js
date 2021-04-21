50c3d3a1eaab57779825f012b2b3e0c9bc7c99cd
Elizabeth <echurinov@gmail.com>
2021-04-21 10:42:22 -0400

    Idk Still trying to get it to work

--- src/Pages/AdminLogin.js
+++ src/Pages/AdminLogin.js
@@ -4,42 +4,25 @@ import AdminTools from "../components/admin/adminTools";
 import "./AdminLogin.css";
 import axios from "axios";
 
-class Account extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: this.props.name,
-      password: this.props.position,
-    };
-  }
-}
-
-class Accounts extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      accounts: [],
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:8082/api/account/")
-      .then((res) => {
-        const accounts = res.data;
-        this.setState({ accounts: accounts });
-      })
-      .catch((err) => {
-        console.log("Error from AdminLogin.js");
-      });
-  }
-}
-
-const accounts = [<Account email="admin@admin.com" password="admin123" />];
-
 function AdminLogin() {
+  const state = {
+    email: '',
+    password: '',
+    accounts:[],
+  }
+
+  axios
+    .get("http://localhost:8082/api/account/")
+    .then((res) => {
+      const accounts = res.data;
+      this.setState({ accounts: accounts });
+    })
+    .catch((err) => {
+      console.log("Error from AdminLogin.js");
+    });
+
+  const accounts = [<Account email="admin@admin.com" password="admin123" />];
+
   const adminUser = {
     email: "admin@admin.com",
     password: "admin123",
