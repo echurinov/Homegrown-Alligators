5b88b7618fe0e37b6ad474f92486f6c216307e57
Elizabeth <echurinov@gmail.com>
2021-04-21 18:33:10 -0400

    Heroku

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 const uri = process.env.MONGODB_URI;
+
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI || db,
-      {
-        useNewUrlParser: true
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI || db, {
+      useNewUrlParser: true,
+    });
 
-    console.log('MongoDB is Connected...');
+    console.log("MongoDB is Connected...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
