c0c8de1e435ee1c0c507d3f8a33e2762ec9d7fe6
Elizabeth <echurinov@gmail.com>
2021-04-21 10:45:48 -0400

    Update AdminLogin.js

--- src/Pages/AdminLogin.js
+++ src/Pages/AdminLogin.js
@@ -2,27 +2,21 @@ import React, { useState } from "react";
 import LoginForm from "../components/admin/loginForm";
 import AdminTools from "../components/admin/adminTools";
 import "./AdminLogin.css";
-import axios from "axios";
+
+class Account extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: this.props.name,
+      password: this.props.position,
+    };
+  }
+}
+
+const accounts = [<Account email="admin@admin.com" password="admin123" />];
 
 function AdminLogin() {
-  const state = {
-    email: '',
-    password: '',
-    accounts:[],
-  }
-
-  axios
-    .get("http://localhost:8082/api/account/")
-    .then((res) => {
-      const accounts = res.data;
-      this.setState({ accounts: accounts });
-    })
-    .catch((err) => {
-      console.log("Error from AdminLogin.js");
-    });
-
-  const accounts = [<Account email="admin@admin.com" password="admin123" />];
-
   const adminUser = {
     email: "admin@admin.com",
     password: "admin123",
@@ -34,7 +28,10 @@ function AdminLogin() {
   const Login = (details) => {
     console.log(details);
 
-    if (accounts.exists({ email: details.email, password: details.password })) {
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
       console.log("Logged in");
       setUser({
         name: details.name,
